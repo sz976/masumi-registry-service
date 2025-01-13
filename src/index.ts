726404ae68e3634d1126b86890b7e6cf29217e77
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-01-13 15:24:49 +0700

    added cors header

--- src/index.ts
+++ src/index.ts
@@ -38,7 +38,7 @@ initialize()
                 "Access-Control-Max-Age": "5000",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH",
-                "Access-Control-Allow-Headers": "Content-Type, Authorization",
+                "Access-Control-Allow-Headers": "*",
                 "Access-Control-Expose-Headers": "Content-Range, X-Total-Count"
             }),
             logger: logger
