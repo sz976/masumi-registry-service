efbb4d2911b68efe677955243e931a27e19b75c3
Sandro Schaier <80533393+schaier-io@users.noreply.github.com>
2025-02-24 14:08:06 +0100

    fix(bakcend): undefined filter tags will properly ignore; naming changes

--- src/repositories/registry-entry/registry-entry.repository.ts
+++ src/repositories/registry-entry/registry-entry.repository.ts
@@ -1,30 +1,48 @@
-import { prisma } from "@/utils/db"
-import { Network, PaymentType, Status } from "@prisma/client";
+import { prisma } from '@/utils/db';
+import { Network, PaymentType, Status } from '@prisma/client';
 
-async function getRegistryEntry(capability: { name: string | undefined; version: string | undefined } | undefined, allowedPaymentTypes: PaymentType[], allowedStatuses: Status[], currentRegistryIdentifier: string | undefined, currentAssetIdentifier: string | undefined, tags: string[] | undefined, currentCursorId: string | undefined, limit: number, network: Network) {
-    return await prisma.registryEntry.findMany({
-        where: {
-            Capability: capability,
-            PaymentIdentifier: { some: { paymentType: { in: allowedPaymentTypes } } },
-            status: { in: allowedStatuses },
-            identifier: currentAssetIdentifier,
-            RegistrySource: { identifier: currentRegistryIdentifier, network: network },
-            tags: { hasSome: tags }
-        },
-        include: {
-            Capability: true,
-            RegistrySource: true,
-            Prices: true,
-        },
-        orderBy: [{
-            createdAt: 'desc',
-        }, {
-            id: "desc"
-        }],
-        cursor: currentCursorId ? { id: currentCursorId } : undefined,
-        //over-fetching to account for health check failures
-        take: (limit * 2)
-    });
+async function getRegistryEntry(
+  capability:
+    | { name: string | undefined; version: string | undefined }
+    | undefined,
+  allowedPaymentTypes: PaymentType[],
+  allowedStatuses: Status[],
+  currentRegistryIdentifier: string | undefined,
+  currentAssetIdentifier: string | undefined,
+  tags: string[] | undefined,
+  currentCursorId: string | undefined,
+  limit: number,
+  network: Network
+) {
+  return await prisma.registryEntry.findMany({
+    where: {
+      Capability: capability,
+      PaymentIdentifier: { some: { paymentType: { in: allowedPaymentTypes } } },
+      status: { in: allowedStatuses },
+      identifier: currentAssetIdentifier,
+      RegistrySource: {
+        identifier: currentRegistryIdentifier,
+        network: network,
+      },
+      tags: tags ? { hasSome: tags } : undefined,
+    },
+    include: {
+      Capability: true,
+      RegistrySource: true,
+      Prices: true,
+    },
+    orderBy: [
+      {
+        createdAt: 'desc',
+      },
+      {
+        id: 'desc',
+      },
+    ],
+    cursor: currentCursorId ? { id: currentCursorId } : undefined,
+    //over-fetching to account for health check failures
+    take: limit * 2,
+  });
 }
 
-export const registryEntryRepository = { getRegistryEntry }  
+export const registryEntryRepository = { getRegistryEntry };
--- src/routes/api/registry-entry/index.ts
+++ src/routes/api/registry-entry/index.ts
@@ -1,88 +1,108 @@
 import { authenticatedEndpointFactory } from '@/utils/endpoint-factory/authenticated';
 import { z } from 'zod';
-import { ez } from "express-zod-api";
+import { ez } from 'express-zod-api';
 import { tokenCreditService } from '@/services/token-credit';
 import { $Enums, Network } from '@prisma/client';
 import { registryEntryService } from '@/services/registry-entry';
 
 export const queryRegistrySchemaInput = z.object({
-    network: z.nativeEnum(Network),
-    limit: z.number({ coerce: true }).int().min(1).max(50).default(10),
-    //optional data
-    cursorId: z.string().min(1).max(50).optional(),
-    filter: z.object({
-        paymentTypes: z.array(z.nativeEnum($Enums.PaymentType)).max(5).optional(),
-        status: z.array(z.nativeEnum($Enums.Status)).max(5).optional(),
-        registryIdentifier: z.string().min(1).max(250).optional(),
-        assetIdentifier: z.string().min(1).max(250).optional(),
-        tags: z.array(z.string().min(1).max(150)).optional(),
-        capability: z.object({ name: z.string().min(1).max(150), version: z.string().max(150).optional() }).optional(),
-    }).optional(),
-    //force refresh
-    minRegistryDate: ez.dateIn().optional(),
-    minHealthCheckDate: ez.dateIn().optional(),
-
-})
+  network: z.nativeEnum(Network),
+  limit: z.number({ coerce: true }).int().min(1).max(50).default(10),
+  //optional data
+  cursorId: z.string().min(1).max(50).optional(),
+  filter: z
+    .object({
+      paymentTypes: z.array(z.nativeEnum($Enums.PaymentType)).max(5).optional(),
+      status: z.array(z.nativeEnum($Enums.Status)).max(5).optional(),
+      policyId: z.string().min(1).max(250).optional(),
+      assetName: z.string().min(1).max(250).optional(),
+      tags: z.array(z.string().min(1).max(150)).optional(),
+      capability: z
+        .object({
+          name: z.string().min(1).max(150),
+          version: z.string().max(150).optional(),
+        })
+        .optional(),
+    })
+    .optional(),
+  //force refresh
+  minRegistryDate: ez.dateIn().optional(),
+  minHealthCheckDate: ez.dateIn().optional(),
+});
 
 export const queryRegistrySchemaOutput = z.object({
-    entries: z.array(z.object(
-        {
-            RegistrySource: z.object({
-                type: z.nativeEnum($Enums.RegistryEntryType),
-                identifier: z.string().nullable(),
-                url: z.string().nullable(),
-            }),
-            Capability: z.object({
-                name: z.string(),
-                version: z.string(),
-                description: z.string().nullable(),
-            }),
-            name: z.string(),
-            description: z.string().nullable(),
-            status: z.nativeEnum($Enums.Status),
-            id: z.string(),
-            lastUptimeCheck: ez.dateOut(),
-            uptimeCount: z.number(),
-            uptimeCheckCount: z.number(),
-            apiUrl: z.string(),
-            authorName: z.string().nullable(),
-            authorOrganization: z.string().nullable(),
-            authorContact: z.string().nullable(),
-            image: z.string().nullable(),
-            privacyPolicy: z.string().nullable(),
-            termsAndCondition: z.string().nullable(),
-            otherLegal: z.string().nullable(),
-            requestsPerHour: z.number().nullable(),
-            tags: z.array(z.string()).nullable(),
-            identifier: z.string(),
-            Prices: z.array(z.object({
-                quantity: z.number(),
-                unit: z.string(),
-            })),
-        }
-    ))
+  entries: z.array(
+    z.object({
+      RegistrySource: z.object({
+        id: z.string(),
+        type: z.nativeEnum($Enums.RegistryEntryType),
+        policyId: z.string().nullable(),
+        url: z.string().nullable(),
+      }),
+      Capability: z.object({
+        name: z.string(),
+        version: z.string(),
+        description: z.string().nullable(),
+      }),
+      name: z.string(),
+      description: z.string().nullable(),
+      status: z.nativeEnum($Enums.Status),
+      id: z.string(),
+      lastUptimeCheck: ez.dateOut(),
+      uptimeCount: z.number(),
+      uptimeCheckCount: z.number(),
+      apiUrl: z.string(),
+      authorName: z.string().nullable(),
+      authorOrganization: z.string().nullable(),
+      authorContact: z.string().nullable(),
+      image: z.string().nullable(),
+      privacyPolicy: z.string().nullable(),
+      termsAndCondition: z.string().nullable(),
+      otherLegal: z.string().nullable(),
+      requestsPerHour: z.number().nullable(),
+      tags: z.array(z.string()).nullable(),
+      agentIdentifier: z.string(),
+      Prices: z.array(
+        z.object({
+          quantity: z.number(),
+          unit: z.string(),
+        })
+      ),
+    })
+  ),
 });
 
 export const queryRegistryEntryPost = authenticatedEndpointFactory.build({
-    method: "post",
-    input: queryRegistrySchemaInput,
-    output: queryRegistrySchemaOutput,
-    handler: async ({ input, options, logger }) => {
-        logger.info("Querying registry", input.paymentTypes);
-        const tokenCost = 0;
-        //TODO update cost model
-        //TODO add custom errors
-        await tokenCreditService.handleTokenCredits(options, tokenCost, "query for: " + input.filter?.capability?.name);
-        const data = await registryEntryService.getRegistryEntries(input);
+  method: 'post',
+  input: queryRegistrySchemaInput,
+  output: queryRegistrySchemaOutput,
+  handler: async ({ input, options, logger }) => {
+    logger.info('Querying registry', input.paymentTypes);
+    const tokenCost = 0;
+    //TODO update cost model
+    //TODO add custom errors
+    await tokenCreditService.handleTokenCredits(
+      options,
+      tokenCost,
+      'query for: ' + input.filter?.capability?.name
+    );
+    const data = await registryEntryService.getRegistryEntries(input);
 
-        return {
-            entries: data.slice(0, Math.min(input.limit, data.length)).map(entry => ({
-                ...entry,
-                Prices: entry.Prices.map(price => ({
-                    ...price,
-                    quantity: Number(price.quantity)
-                }))
-            }))
-        }
-    },
+    return {
+      entries: data
+        .slice(0, Math.min(input.limit, data.length))
+        .map((entry) => ({
+          ...entry,
+          RegistrySource: {
+            ...entry.RegistrySource,
+            policyId: entry.RegistrySource.identifier,
+          },
+          agentIdentifier: entry.identifier,
+          Prices: entry.Prices.map((price) => ({
+            ...price,
+            quantity: Number(price.quantity),
+          })),
+        })),
+    };
+  },
 });
